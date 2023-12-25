075181abab56e5c2923c2c893859705bba3369d4
omar <ajimiomar.oa@gmail.com>
2023-12-25 01:29:41 +0100

    Initial commit

---
+++ src/components/navbar.js
@@ -0,0 +1,46 @@
+
+'use client';
+
+import { Avatar, Dropdown, Navbar } from 'flowbite-react';
+
+function Nav() {
+  return (
+    <Navbar fluid rounded>
+     <Navbar.Brand href="#">
+     <img src="/assets/img/logo.png" className=" max-w-28" alt="Logo" />  
+     <span>hello Mr {}</span> 
+</Navbar.Brand>
+
+      <div className="flex md:order-">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar alt="User settings" img="/assets/img/sdouga.jpg" rounded />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="#" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="#">About</Navbar.Link>
+        <Navbar.Link href="#">Services</Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Contact</Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+export default Nav 
